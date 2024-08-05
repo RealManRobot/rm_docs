a9cb43972f498b0e80d30c4d57b48d64992ec998
jiangpengyu@realman-robot.com <jiangpengyu@realman-robot.com>
2024-08-05 15:21:17 +0800

    添加百度统计

--- docs/.vitepress/head/index.ts
+++ docs/.vitepress/head/index.ts
@@ -3,4 +3,15 @@ export const head: HeadConfig[] = [
   ["link", { rel: "icon", href: "/assets/logo.png" }],
   ["link", { rel: "stylesheet", href: "/css/index.css" }],
   ["meta", { name: "keywords", content: "睿尔曼智能科技" }],
+  [
+    "script",
+    {},
+    `var _hmt = _hmt || [];
+    (function() {
+      var hm = document.createElement("script");
+      hm.src = "https://hm.baidu.com/hm.js?4e8bfd1617a02c2841e7282af583cc0f";
+      var s = document.getElementsByTagName("script")[0]; 
+      s.parentNode.insertBefore(hm, s);
+    })();`,
+  ],
 ];
