da99489d8ed2a63c83923bd54df213156f2aff37
jiangpengyu@realman-robot.com <jiangpengyu@realman-robot.com>
2024-09-03 09:38:22 +0800

    调整搜索

--- docs/.vitepress/themes/index.ts
+++ docs/.vitepress/themes/index.ts
@@ -76,52 +76,6 @@ export const themeConfig: DefaultTheme.Config = {
     //     },
     //   },
     // },
-    provider: "algolia",
-    options: {
-      appId: "SV1S7E1V17",
-      apiKey: "98077eb74fe04293108c60a5ba47dc3f",
-      // apiKey: "98077eb74fe04293108c60a5ba47dc3f",
-      indexName: "develop-realman-robotics",
-      placeholder: "搜索文档",
-      translations: {
-        button: {
-          buttonText: "搜索文档",
-          buttonAriaLabel: "搜索文档",
-        },
-        modal: {
-          searchBox: {
-            resetButtonTitle: "清除查询条件",
-            resetButtonAriaLabel: "清除查询条件",
-            cancelButtonText: "取消",
-            cancelButtonAriaLabel: "取消",
-          },
-          startScreen: {
-            recentSearchesTitle: "搜索历史",
-            noRecentSearchesText: "没有搜索历史",
-            saveRecentSearchButtonTitle: "保存至搜索历史",
-            removeRecentSearchButtonTitle: "从搜索历史中移除",
-            favoriteSearchesTitle: "收藏",
-            removeFavoriteSearchButtonTitle: "从收藏中移除",
-          },
-          errorScreen: {
-            titleText: "无法获取结果",
-            helpText: "你可能需要检查你的网络连接",
-          },
-          footer: {
-            selectText: "选择",
-            navigateText: "切换",
-            closeText: "关闭",
-            searchByText: "搜索提供者",
-          },
-          noResultsScreen: {
-            noResultsText: "无法找到相关结果",
-            suggestedQueryText: "你可以尝试查询",
-            reportMissingResultsText: "你认为该查询应该有结果？",
-            reportMissingResultsLinkText: "点击反馈",
-          },
-        },
-      },
-    },
   },
   docFooter: {
     prev: "上一篇",
