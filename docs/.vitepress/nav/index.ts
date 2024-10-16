495898b4e57f1a1d7389a68eaaea54f37a870298
Jacky <jackyzhu@realman-robot.com>
2024-10-16 21:28:42 +0800

    更新 Json 3.7.4 协议说明

--- docs/.vitepress/nav/index.ts
+++
@@ -1,9 +0,0 @@
-import type { DefaultTheme } from "vitepress";
-export const navsList: DefaultTheme.NavItem[] = [
-  // { text: "Python", link: "/python/index", activeMatch: "/python/" },
-  { text: "机械臂", link: "/robot/summarize/", activeMatch: "/robot/" },
-  //{ text: "关节", link: "/joints/summarize/index.md", activeMatch: "/joints/" },
-  // { text: "复合机器人", link: "/compositeRobot/index", activeMatch: "/compositeRobot/" },
-  { text: "AI", link: "/AI/getStarted/overview.md", activeMatch: "/AI/" },
-  //{ text: "生态", link: "/symbiosis/getStarted/index.md", activeMatch: "/symbiosis/" },
-];
